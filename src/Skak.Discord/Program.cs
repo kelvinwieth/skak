08ae9d862ba1102133a673150bef5706a93fc05b
kelvinwieth <kelvin.wieth@partner.bmw.com.br>
2022-09-18 02:47:15 -0300

    simplify program

--- src/Skak.Discord/Program.cs
+++ src/Skak.Discord/Program.cs
@@ -1,7 +1,1 @@
-﻿using System.Text;
-
-var path = "~/test.txt";
-
-using var file = File.Create(path);
-byte[] info = new UTF8Encoding(true).GetBytes("hello");
-file.Write(info, 0, info.Length);
+﻿Console.WriteLine("Hello, world!");
