a2f3f4837c3efc92f6e3efce9c1ad0ee93397328
kelvinwieth <wiethkelvin@gmail.com>
2022-09-25 17:29:15 -0300

    chore: docker prune after run

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,3 +2,4 @@
 docker ps -f name=skak -q | xargs --no-run-if-empty docker container stop
 docker container ls -a -fname=skak -q | xargs -r docker container rm
 docker run -d -e SKAK_TOKEN=$SKAK_TOKEN --name skak skak:latest
+docker system prune -f
