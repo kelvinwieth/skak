9a0691b67ee57fb32b6901311f712667adc14836
Kelvin Wieth <79933699+kelvinwieth@users.noreply.github.com>
2023-01-09 01:06:57 -0300

    refactor: remove refit and use own lichess client

---
+++ src/Skak.Discord/Clients/LichessRoutes.cs
@@ -0,0 +1,31 @@
+﻿using Skak.Discord.Models.Enums;
+
+namespace Skak.Discord.Clients
+{
+    public static class LichessRoutes
+    {
+        public const string BaseUrl = "https://lichess.org/api";
+
+        public const string TeamName = "chessclub-brpt";
+
+        public static string SwissTournaments(int maxTournaments)
+        {
+            return $"{BaseUrl}/team/{TeamName}/swiss?max={maxTournaments}";
+        }
+
+        public static string ArenaTournaments(int maxTournaments)
+        {
+            return $"{BaseUrl}/team/{TeamName}/arena?max={maxTournaments}";
+        }
+
+        public static string TeamTournaments(TournamentType type, int maxTournaments)
+        {
+            return type switch
+            {
+                TournamentType.Swiss => SwissTournaments(maxTournaments),
+                TournamentType.Arena => ArenaTournaments(maxTournaments),
+                _ => throw new ArgumentException("Unsupported tournament type"),
+            };
+        }
+    }
+}
