a97e4ad00a2090d22e0501e8c9fb1057253e735c
Kelvin Wieth <79933699+kelvinwieth@users.noreply.github.com>
2023-01-15 14:38:00 -0300

    feat: add string extension to uft8

---
+++ src/Skak.Discord/Utils/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Skak.Discord.Utils
+{
+    public static class StringExtensions
+    {
+        public static string ToUft8String(this string value)
+        {
+            var bytes = Encoding.Default.GetBytes(value);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
