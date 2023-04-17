b84c4fb69b25d85824fac5e47cf4b2d18a17eef1
kelvinwieth <wiethkelvin@gmail.com>
2023-04-17 19:57:33 -0300

    fix: adjust pt timezone

--- src/Skak.Discord/Utils/DateTimeExtensions.cs
+++ src/Skak.Discord/Utils/DateTimeExtensions.cs
@@ -12,7 +12,7 @@
         public static DateTime ToLisboa(this DateTime dateTime)
         {
             var utc = dateTime.ToUniversalTime();
-            var lisboaInfo = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            var lisboaInfo = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
             return TimeZoneInfo.ConvertTimeFromUtc(utc, lisboaInfo);
         }
     }
